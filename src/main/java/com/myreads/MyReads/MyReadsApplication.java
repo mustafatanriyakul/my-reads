5ef1554b510dada845c593c643a1d93ca602b699
Leo1526 <mustafatanriyakul1@gmail.com>
2025-08-03 14:28:05 +0300

    spring boot project setup

---
+++ src/main/java/com/myreads/MyReads/MyReadsApplication.java
@@ -0,0 +1,18 @@
+package com.myreads.MyReads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyReadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyReadsApplication.class, args);
+
+
+
+
+
+	}
+
+}
