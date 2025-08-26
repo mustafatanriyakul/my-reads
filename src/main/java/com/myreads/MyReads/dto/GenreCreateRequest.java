4e87a8608b2c693321bc79da4723a6fe7a5c99d9
mustafa.tanriyakul <mustafatanriyakul1@gmail.com>
2025-08-26 20:29:28 +0300

    -Genre and AuthorGenre created.

---
+++ src/main/java/com/myreads/MyReads/dto/GenreCreateRequest.java
@@ -0,0 +1,10 @@
+package com.myreads.MyReads.dto;
+
+import lombok.Data;
+
+@Data
+public class GenreCreateRequest {
+
+    private String name;
+
+}
---
+++ src/main/java/com/myreads/MyReads/models/Genre.java
@@ -0,0 +1,36 @@
+package com.myreads.MyReads.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@EntityListeners(AuditingEntityListener.class)
+public class Genre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDate updatedAt;
+
+    public Genre(String name){
+        this.name = name;
+    }
+
+    public Genre(){
+
+    }
+}
